991fcd0147fa3c5670b517598d9672fa56ab6736
Kiryl Vasko <kirill.vasko.2003@gmail.com>
2022-07-04 15:39:56 +0300

    Delete PostDeploymentSql.sql

--- MeetupDB/PostDeploymentSql.sql
+++
@@ -1,7 +0,0 @@
-﻿:r .\Scripts\Users.sql
-:r .\Scripts\Roles.sql
-:r .\Scripts\Organizers.sql
-:r .\Scripts\Speakers.sql
-:r .\Scripts\EventsScript.sql
-:r .\Scripts\EventOrganizer.sql
-:r .\Scripts\EventSpeaker.sql
